7b7cafa75cc513cad99acb873f5f2eeee42c46e3
Govind Gupta <govind12771277@gmail.com>
2025-05-20 23:48:39 +0530

    knowledge_graph

--- class8-knowledge_graph/main.py
+++ class8-knowledge_graph/main.py
@@ -1,69 +1,73 @@
-# from mem0 import Memory
+from mem0 import Memory
 from openai import OpenAI
+from colorama import Fore, Style
 
-# GEMINI_API_KEY = "AIzaSyBfbdVV0YNmWCK2p5q_qnWzKHwdMlu0T5s"
+OPENAI_API_KEY = "sk-proj-oLTALbCqFOkKiehWtHIwZIcdjE7McASdZmJQghvh4OHHeMmH4hnGlE0JGRahUdcdvelWR0tkA"
 
-# QUADRANT_HOST = "localhost"
+QUADRANT_HOST = "localhost"
 
-# NEO4J_URL = "bolt://localhost:7687"
-# NEO4J_USERNAME = "neo4j"
-# NEO4J_PASSWORD = "reform-william-center-vibrate-press-5829"
+NEO4J_URL = "bolt://localhost:7687"
+NEO4J_USERNAME = "neo4j"
+NEO4J_PASSWORD = "reform-william-center-vibrate-press-5829"
 
-# config = {
-#     "version": "v1.1",
-#     "embedder": {
-#         "provider": "google",
-#         "config": {
-#             "api_key": GEMINI_API_KEY,
-#             "model": "models/text-embedding-004"  # Gemini embedding model
-#         },
-#     },
-#     "llm": {
-#         "provider": "google",
-#         "config": {
-#             "api_key": GEMINI_API_KEY,
-#             "model": "gemini-2.0-flash"  # Gemini LLM
-#         },
-#     },
-#     "vector_store": {
-#         "provider": "qdrant",
-#         "config": {
-#             "host": QUADRANT_HOST,
-#             "port": 6333,
-#         },
-#     },
-#     "graph_store": {
-#         "provider": "neo4j",
-#         "config": {
-#             "url": NEO4J_URL,
-#             "username": NEO4J_USERNAME,
-#             "password": NEO4J_PASSWORD,
-#         },
-#     },
-# }
+config = {
+    "version": "v1.1",
+    "embedder": {
+        "provider": "openai",
+        "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"},
+    },
+    "llm": {"provider": "openai", "config": {"api_key": OPENAI_API_KEY, "model": "gpt-4.1"}},
+    "vector_store": {
+        "provider": "qdrant",
+        "config": {
+            "host": QUADRANT_HOST,
+            "port": 6333,
+        },
+    },
+    "graph_store": {
+        "provider": "neo4j",
+        "config": {"url": NEO4J_URL, "username": NEO4J_USERNAME, "password": NEO4J_PASSWORD},
+    },
+}
 
-# mem_client = Memory.from_config(config)
 
-OpenAI_client = OpenAI(
-    api_key="AIzaSyD1MkJaYvnWoh5EG2dzb8pvkcmNQwtMcIw",
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-)
+mem_client = Memory.from_config(config)
+
+OpenAI_client = OpenAI(api_key=OPENAI_API_KEY)
+
 
 def chat(message):
-    messages = [{"role": "user", "content": message}]
 
+    mem_result = mem_client.search(query=message, user_id="1234")
+    print(f'{Fore.MAGENTA}{Style.DIM} mem : {mem_result} {Style.RESET_ALL}')
+
+    memories = "\n".join([m["memory"] for m in mem_result.get("results")])
+
+    print(f'{Fore.BLUE}{Style.DIM} memories : {memories} {Style.RESET_ALL}')
+
+    SYSTEM_PROMPT = f"""You are a helpful assistant that answers questions based on the provided context.
+    Memory and it's score : {memories}
+    """
+    messages = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": message}
+    ]
     response = OpenAI_client.chat.completions.create(
-        model="gemini-2.0-flash",
-        response_format={"type": "json_object"},
+        model="gpt-4.1",
         messages=messages
     )
 
-    messages.append({"role": "assistant", "content": response.choices[0].message.content})
+    messages.append(
+        {"role": "assistant", "content": response.choices[0].message.content})
+
+    mem_client.add(messages, user_id="1234")
 
     return response.choices[0].message.content
 
 
 while True:
     user_input = input(">> ")
+    if (user_input.lower() == "exit"):
+        break
     response = chat(user_input)
     print("Bot:", response)
